f0f4e7bb3c498c346ff6cffc3e91ce7c4991a3a2
kirill5k <immotional@aol.com>
2020-10-22 10:40:39 +0100

    clients module

---
+++ core/src/main/scala/ebayapp/clients/Clients.scala
@@ -0,0 +1,29 @@
+package ebayapp.clients
+
+import cats.effect.{Concurrent, Timer}
+import cats.implicits._
+import ebayapp.clients.cex.CexClient
+import ebayapp.clients.ebay.EbayClient
+import ebayapp.clients.telegram.TelegramClient
+import ebayapp.common.config.AppConfig
+import io.chrisdavenport.log4cats.Logger
+import sttp.client.{NothingT, SttpBackend}
+
+final case class Clients[F[_]](
+    ebay: EbayClient[F],
+    cex: CexClient[F],
+    telegram: TelegramClient[F]
+)
+
+object Clients {
+
+  def make[F[_]: Concurrent: Timer: Logger](
+      config: AppConfig,
+      backend: SttpBackend[F, Nothing, NothingT]
+  ): F[Clients[F]] = {
+    val ebay = EbayClient.make[F](config.ebay, backend)
+    val cex = CexClient.make[F](config.cex, backend)
+    val telegram = TelegramClient.make[F](config.telegram, backend)
+    (ebay, cex, telegram).mapN(Clients.apply)
+  }
+}
