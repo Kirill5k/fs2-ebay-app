2dafa2a6fb237651fd7a09f630068b53cdebf66f
kirill5k <immotional@aol.com>
2020-10-23 16:31:00 +0100

    EbayDealsFinder test spec

--- core/src/test/scala/ebayapp/tasks/EbayDealsFinderSpec.scala
+++ core/src/test/scala/ebayapp/tasks/EbayDealsFinderSpec.scala
@@ -1,10 +1,14 @@
 package ebayapp.tasks
 
 import cats.effect.IO
+import cats.implicits._
 import ebayapp.CatsSpec
+import ebayapp.clients.ebay.mappers.EbayItemMapper
+import ebayapp.clients.ebay.search.EbaySearchParams
 import ebayapp.common.config.{EbayDealsConfig, EbayDealsConfigs, SearchQuery}
 import ebayapp.domain.ItemDetails
-import ebayapp.services.{EbayDealsService, NotificationService, ResellableItemService}
+import ebayapp.domain.ItemDetails.Game
+import ebayapp.services.{CexStockService, EbayDealsService, NotificationService, ResellableItemService, Services}
 
 import scala.concurrent.duration._
 
@@ -13,13 +17,33 @@ class EbayDealsFinderSpec extends CatsSpec {
   val searchQueries = List(SearchQuery("q1"), SearchQuery("q1"))
   val dealConfig    = EbayDealsConfigs(EbayDealsConfig(3.seconds, Nil, 20.minutes, 34))
 
-  "A VideoGamesEbayDealsFinder" should {}
+  implicit val mapper: EbayItemMapper[Game] = EbayItemMapper.gameDetailsMapper
+  implicit val params: EbaySearchParams[Game] = EbaySearchParams.videoGameSearchParams
 
+  "A VideoGamesEbayDealsFinder" should {
 
-  def mocks: (EbayDealsService[IO], ResellableItemService[IO, ItemDetails.Game], NotificationService[IO]) = {
-    val ebay         = mock[EbayDealsService[IO]]
-    val item         = mock[ResellableItemService[IO, ItemDetails.Game]]
-    val notification = mock[NotificationService[IO]]
-    (ebay, item, notification)
+    "retry forever periodically" in {
+      val services = mocks
+      when(services.ebayDeals.find(any[SearchQuery], any[FiniteDuration])(any[EbayItemMapper[ItemDetails.Game]], any[EbaySearchParams[ItemDetails.Game]]))
+        .thenReturn(fs2.Stream.empty)
+
+      val result = for {
+        task <- EbayDealsFinder.videoGames(dealConfig, services)
+        _    <- task.searchForCheapItems().compile.drain
+        _    <- IO.sleep(10.seconds)
+      } yield task
+
+      result.unsafeToFuture().map { r =>
+        verify(services.ebayDeals).find(any[SearchQuery], eqTo(20.minutes))(eqTo(mapper), eqTo(params))
+        r must be (())
+      }
+    }
   }
+
+  def mocks: Services[IO] = Services[IO](
+    mock[NotificationService[IO]],
+    mock[ResellableItemService[IO, ItemDetails.Game]],
+    mock[EbayDealsService[IO]],
+    mock[CexStockService[IO, ItemDetails.Generic]]
+  )
 }
