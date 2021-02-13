1aa2d9e8d6b39f0b06c427dea07c4abbcf7e3e08
kirill5k <immotional@aol.com>
2021-02-13 10:47:35 +0000

    refactor error handling in tasks 3

--- core/src/main/scala/ebayapp/tasks/Tasks.scala
+++ core/src/main/scala/ebayapp/tasks/Tasks.scala
@@ -12,22 +12,24 @@ import scala.concurrent.duration._
 final case class Tasks[F[_]: Concurrent: Logger: Timer](
     tasks: List[Task[F]]
 ) {
+  import Tasks._
+
   def runAll: Stream[F, Unit] =
     Stream
       .emits(tasks)
       .map(_.run().resumeOnError(1.minute))
       .parJoinUnbounded
+}
 
-  implicit final private class StreamOps[O](private val stream: Stream[F, O]) extends AnyVal {
+object Tasks {
+
+  implicit final private[tasks] class StreamOps[F[_], O](private val stream: Stream[F, O]) extends AnyVal {
     def resumeOnError(delay: FiniteDuration)(implicit logger: Logger[F], timer: Timer[F]): Stream[F, O] =
       stream.handleErrorWith { error =>
         Stream.eval_(logger.critical(error)("error during task processing")) ++
           stream.delayBy(delay)(timer)
       }
   }
-}
-
-object Tasks {
 
   def make[F[_]: Concurrent: Logger: Timer](config: AppConfig, services: Services[F]): F[Tasks[F]] =
     List(
