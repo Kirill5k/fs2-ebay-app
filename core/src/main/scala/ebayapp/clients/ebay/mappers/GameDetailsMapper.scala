6dd88aaa231380ec2b5630877e445a13cb983d82
kirill5k <immotional@aol.com>
2021-02-17 18:21:59 +0000

    update to game details mapper

--- core/src/main/scala/ebayapp/clients/ebay/mappers/GameDetailsMapper.scala
+++ core/src/main/scala/ebayapp/clients/ebay/mappers/GameDetailsMapper.scala
@@ -62,7 +62,7 @@ private[mappers] object GameDetailsMapper {
     "((new|all) )?(fully )?(((very|super) )?rare|strictly limited|exclusive|limited run|(\\d+ )?new|pal|physical|great|boxed|full|two|complete|boxed complete) game(s)?( (\\d+|in one))?( new)?",
     "(\\b(\\d player|kids( \\w+)?|multiplayer|football sport|rally|driving|shooting|fighting|hacker|(car )?racing|Skateboarding|action|hit|official|strategy|console|gold|(base )?main|children)\\b.{0,15})?(video( )?)?game(s)?( (for kids|series|film|racing|good|boxed|collection|console|of (year|olympic(s)?|movie)))?( 20\\d\\d)?",
     // removes the word USED
-    "((barely|condition|never|hardly) )?(un)?used( (once|twice))?(( very)? good)?( (game(s)?|condition))?",
+    "((barely|condition|never|hardly|dcl) )?(un)?used( (once|twice))?(( very)? good)?( (game(s)?|condition))?",
     // removes the word VERSION
     "(official|20\\d\\d) version",
     // removes the word "BRAND NEW"
