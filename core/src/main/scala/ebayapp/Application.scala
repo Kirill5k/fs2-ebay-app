91a91bb8e682333b5deea28a90a633f791d69b24
kirill5k <immotional@aol.com>
2021-03-02 20:29:42 +0000

    gracefully stop application

--- core/src/main/scala/ebayapp/Application.scala
+++ core/src/main/scala/ebayapp/Application.scala
@@ -38,6 +38,7 @@ object Application extends IOApp {
               .interruptWhen(logger.awaitSigTerm)
               .compile
               .drain
+              .attempt
             _ <- logger.info("stopping ebay-app")
             _ <- runningTasks.cancel *> IO.sleep(10.seconds)
           } yield ()
