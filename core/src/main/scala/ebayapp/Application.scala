e0d7b35f40c0023518fc33a84a59cefc33624581
kirill5k <immotional@aol.com>
2021-03-03 08:43:10 +0000

    remove unused val
    remove unused val

--- core/src/main/scala/ebayapp/Application.scala
+++ core/src/main/scala/ebayapp/Application.scala
@@ -12,7 +12,6 @@ import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
 
 object Application extends IOApp {
 
@@ -29,7 +28,7 @@ object Application extends IOApp {
             services     <- Services.make(clients, repositories) <* logger.info("created services")
             tasks        <- Tasks.make(config, services) <* logger.info("created tasks")
             controllers  <- Controllers.make(resources.blocker, services) <* logger.info("created controllers")
-            runningTasks <- logger.info("initiating tasks") *> tasks.runAll.compile.drain.start
+            _            <- logger.info("initiating tasks") *> tasks.runAll.compile.drain.start
             _            <- logger.info("starting http server")
             _ <- BlazeServerBuilder[IO](ExecutionContext.global)
               .bindHttp(config.server.port, config.server.host)
