8ef065083e3c6ea191bf2d21fa9a63fa78d83ee0
kirill5k <immotional@aol.com>
2021-03-11 13:18:31 +0000

    update dependencies

--- core/src/main/scala/ebayapp/clients/ebay/mappers/GameDetailsMapper.scala
+++ core/src/main/scala/ebayapp/clients/ebay/mappers/GameDetailsMapper.scala
@@ -51,6 +51,7 @@ private[mappers] object GameDetailsMapper {
     "(?<=Fifa 21.*) (NXT LVL|Next level)", "(?<=PGA Tour.*) golf.*",
     "(?<=Gran)d(?= turismo)", "(?<=No Mans Sky) Beyond", "(?<=Grand Theft Auto (\\d|\\b[VI]{1,2}\\b)).*map.*",
     "\\bGT(S|\\d)?\\b .*(?=gran turismo)", "(?<=Turismo( (\\d|sport))?) \\bGT(\\d|S)?\\b", "(?<=gears) of war(?= 5)",
+    "bandicoot( CTR)? (?=team racing)",
     "(?<=Sonic) Hedgehog", "Formula (1|One)\\s+(?=F1)", "Marvel(s)?\\s+(?=(iron man|deadpool|Spider))",
     "(?<=\\b[ivx]{1,4}\\b)(\\s+)\\d+", "(?<=\\d) \\b[ivx]{1,4}\\b", "(?<=1) \\bone\b",
     // removes year from some titles
