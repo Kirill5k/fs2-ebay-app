b845906b143b41a7102eef146de4b705f0c45abd
kirill5k <immotional@aol.com>
2021-04-11 19:11:21 +0100

    add more headers

--- core/src/main/scala/ebayapp/clients/jdsports/JdsportsClient.scala
+++ core/src/main/scala/ebayapp/clients/jdsports/JdsportsClient.scala
@@ -27,6 +27,10 @@ final private class LiveJdsportsClient[F[_]](
 ) extends JdsportsClient[F] {
 
   private val defaultHeaders: Map[String, String] = Map(
+    "Connection" -> "keep-alive",
+    "Accept" -> "*/*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Cache-Control" -> "no-cache",
     "User-Agent" -> "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
     "Referer"    -> "https://www.jdsports.co.uk/men/brand/"
   )
