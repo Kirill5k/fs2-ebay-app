389a9bf243b5256f0b4e469ed22b9a5879be113d
kirill5k <immotional@aol.com>
2021-04-15 13:49:47 +0100

    increase connection timeout

--- core/src/main/scala/ebayapp/core/common/Resources.scala
+++ core/src/main/scala/ebayapp/core/common/Resources.scala
@@ -4,8 +4,10 @@ import cats.effect.{Blocker, Concurrent, ContextShift, Resource}
 import cats.implicits._
 import ebayapp.core.common.config.{AppConfig, MongoConfig}
 import mongo4cats.client.MongoClientF
-import sttp.client3.SttpBackend
+import sttp.client3.{SttpBackend, SttpBackendOptions}
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
+
+import scala.concurrent.duration._
 
 trait Resources[F[_]] {
   def blocker: Blocker
@@ -19,7 +21,7 @@ object Resources {
     MongoClientF.fromConnectionString[F](config.connectionUri)
 
   private def httpClientBackend[F[_]: Concurrent: ContextShift]: Resource[F, SttpBackend[F, Any]] =
-    Resource.make(AsyncHttpClientCatsBackend[F]())(_.close())
+    Resource.make(AsyncHttpClientCatsBackend[F](SttpBackendOptions.Default.connectionTimeout(3.minutes)))(_.close())
 
   def make[F[_]: Concurrent: ContextShift](config: AppConfig): Resource[F, Resources[F]] =
     (
