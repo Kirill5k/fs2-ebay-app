a4d83a5f9bfab18e0a0f323496b6690f4cd8095e
kirill5k <immotional@aol.com>
2021-04-24 09:15:34 +0100

    retry on 429 in telegram client

--- core/src/main/scala/ebayapp/core/clients/telegram/TelegramClient.scala
+++ core/src/main/scala/ebayapp/core/clients/telegram/TelegramClient.scala
@@ -1,11 +1,14 @@
 package ebayapp.core.clients.telegram
 
-import cats.MonadError
+import cats.effect.Temporal
 import cats.implicits._
 import ebayapp.core.common.config.TelegramConfig
 import ebayapp.core.common.errors.AppError
 import ebayapp.core.common.Logger
 import sttp.client3._
+import sttp.model.StatusCode
+
+import scala.concurrent.duration._
 
 trait TelegramClient[F[_]] {
   def sendMessageToMainChannel(message: String): F[Unit]
@@ -13,11 +16,11 @@ trait TelegramClient[F[_]] {
   def sendMessageToAlertsChannel(message: String): F[Unit]
 }
 
-final private class TelegramApiClient[F[_]](
+final private class LiveTelegramClient[F[_]](
     private val config: TelegramConfig,
     private val backend: SttpBackend[F, Any]
 )(implicit
-    F: MonadError[F, Throwable],
+    F: Temporal[F],
     logger: Logger[F]
 ) extends TelegramClient[F] {
 
@@ -36,7 +39,10 @@ final private class TelegramApiClient[F[_]](
       .send(backend)
       .flatMap { r =>
         r.body match {
-          case Right(_) => ().pure[F]
+          case Right(_) =>
+            ().pure[F]
+          case Left(_) if r.code == StatusCode.TooManyRequests =>
+            F.sleep(10.seconds) *> sendMessage(channelId, message)
           case Left(error) =>
             logger.warn(s"error sending message to telegram: ${r.code}\n$error") *>
               AppError.Http(r.code.code, s"error sending message to telegram channel $channelId: ${r.code}").raiseError[F, Unit]
@@ -49,6 +55,6 @@ object TelegramClient {
   def make[F[_]: Logger](
       config: TelegramConfig,
       backend: SttpBackend[F, Any]
-  )(implicit F: MonadError[F, Throwable]): F[TelegramClient[F]] =
-    F.pure(new TelegramApiClient[F](config, backend))
+  )(implicit F: Temporal[F]): F[TelegramClient[F]] =
+    F.pure(new LiveTelegramClient[F](config, backend))
 }
--- core/src/test/scala/ebayapp/core/clients/telegram/TelegramClientSpec.scala
+++ core/src/test/scala/ebayapp/core/clients/telegram/TelegramClientSpec.scala
@@ -5,20 +5,22 @@ import ebayapp.core.SttpClientSpec
 import ebayapp.core.common.config.TelegramConfig
 import ebayapp.core.common.errors.AppError
 import sttp.client3.{Response, SttpBackend}
-import sttp.model.{Method, StatusCode}
+import sttp.model.StatusCode
+import ebayapp.core.requests._
 
 class TelegramClientSpec extends SttpClientSpec {
 
   val message = "lorem ipsum dolor sit amet"
-  val config = TelegramConfig("http://telegram.com", "BOT-KEY", "m1", "m2", "alerts")
+  val config  = TelegramConfig("http://telegram.com", "BOT-KEY", "m1", "m2", "alerts")
 
   "TelegramClient" should {
+
+    val sendMessageUrl = "telegram.com/botBOT-KEY/sendMessage"
 
     "send message to the main channel" in {
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
-          case r
-            if isGoingTo(r, Method.GET, "telegram.com", List("botBOT-KEY", "sendMessage"), Map("chat_id" -> "m1", "text" -> message)) =>
+          case r if r.isGet && r.isGoingTo(sendMessageUrl) && r.hasParams(Map("chat_id" -> "m1", "text" -> message)) =>
             Response.ok("success")
           case _ => throw new RuntimeException()
         }
@@ -33,8 +35,7 @@ class TelegramClientSpec extends SttpClientSpec {
     "send message to the secondary channel" in {
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
-          case r
-            if isGoingTo(r, Method.GET, "telegram.com", List("botBOT-KEY", "sendMessage"), Map("chat_id" -> "m2", "text" -> message)) =>
+          case r if r.isGet && r.isGoingTo(sendMessageUrl) && r.hasParams(Map("chat_id" -> "m2", "text" -> message)) =>
             Response.ok("success")
           case _ => throw new RuntimeException()
         }
@@ -49,8 +50,7 @@ class TelegramClientSpec extends SttpClientSpec {
     "send message to the alerts channel" in {
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
-          case r
-            if isGoingTo(r, Method.GET, "telegram.com", List("botBOT-KEY", "sendMessage"), Map("chat_id" -> "alerts", "text" -> message)) =>
+          case r if r.isGet && r.isGoingTo(sendMessageUrl) && r.hasParams(Map("chat_id" -> "alerts", "text" -> message)) =>
             Response.ok("success")
           case _ => throw new RuntimeException()
         }
@@ -65,8 +65,7 @@ class TelegramClientSpec extends SttpClientSpec {
     "send message to the channel" in {
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
-          case r
-            if isGoingTo(r, Method.GET, "telegram.com", List("botBOT-KEY", "sendMessage"), Map("chat_id" -> "m1", "text" -> message)) =>
+          case r if r.isGet && r.isGoingTo(sendMessageUrl) && r.hasParams(Map("chat_id" -> "m1", "text" -> message)) =>
             Response.ok("success")
           case _ => throw new RuntimeException()
         }
@@ -78,11 +77,24 @@ class TelegramClientSpec extends SttpClientSpec {
       result.unsafeToFuture().map(_ must be(()))
     }
 
+    "retry on 429" in {
+      val testingBackend: SttpBackend[IO, Any] = backendStub.whenAnyRequest
+        .thenRespondCyclicResponses(
+          Response("too-many-requests", StatusCode.TooManyRequests),
+          Response.ok("ok")
+        )
+
+      val telegramClient = TelegramClient.make(config, testingBackend)
+
+      val result = telegramClient.flatMap(_.sendMessageToSecondaryChannel(message))
+
+      result.unsafeToFuture().map(_ must be(()))
+    }
+
     "return error when not success" in {
       val testingBackend: SttpBackend[IO, Any] = backendStub
         .whenRequestMatchesPartial {
-          case r
-            if isGoingTo(r, Method.GET, "telegram.com", List("botBOT-KEY", "sendMessage"), Map("chat_id" -> "m1", "text" -> message)) =>
+          case r if r.isGet && r.isGoingTo(sendMessageUrl) && r.hasParams(Map("chat_id" -> "m1", "text" -> message)) =>
             Response("fail", StatusCode.BadRequest)
           case _ => throw new RuntimeException()
         }
