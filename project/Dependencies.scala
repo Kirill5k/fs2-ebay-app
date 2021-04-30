a4bff8d029e62442761e1e4c37a08b51b3a5317f
kirill5k <immotional@aol.com>
2021-04-30 08:09:07 +0100

    dependency updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    lazy val mongo4cats = "0.2.4"
+    lazy val mongo4cats = "0.2.5"
     lazy val pureConfig = "0.15.0"
     lazy val circe      = "0.13.0"
-    lazy val sttp       = "3.3.0-RC4"
+    lazy val sttp       = "3.3.0"
     lazy val http4s     = "1.0.0-M21"
     lazy val logback    = "1.2.3"
-    lazy val log4cats   = "2.0.1"
+    lazy val log4cats   = "2.1.0"
 
     lazy val scalaTest     = "3.2.8"
     lazy val mockito       = "1.16.37"
