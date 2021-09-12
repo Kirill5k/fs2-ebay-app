2add03474c363a303ae29f3a38a8b51193dab9a2
kirill5k <immotional@aol.com>
2021-09-12 10:30:42 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,9 @@ object Dependencies {
     lazy val mongo4cats = "0.4.0"
     lazy val pureConfig = "0.16.0"
     lazy val circe      = "0.14.1"
-    lazy val sttp       = "3.3.13"
+    lazy val sttp       = "3.3.14"
     lazy val http4s     = "1.0.0-M25"
-    lazy val logback    = "1.2.5"
+    lazy val logback    = "1.2.6"
     lazy val log4cats   = "2.1.1"
 
     lazy val scalaTest = "3.2.9"
