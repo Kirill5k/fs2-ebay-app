fc89e0be60314556eedb2fdd6ceee15cdea78237
kirill5k <immotional@aol.com>
2021-11-20 12:39:11 +0000

    Include req metadata in health

--- core/src/main/scala/ebayapp/core/controllers/HealthController.scala
+++ core/src/main/scala/ebayapp/core/controllers/HealthController.scala
@@ -6,13 +6,24 @@ import io.circe.generic.auto._
 import org.http4s.HttpRoutes
 import sttp.capabilities.fs2.Fs2Streams
 import sttp.tapir._
+import sttp.tapir.model.ServerRequest
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-final case class AppStatus(status: true)
+final case class Metadata(uri: String, headers: Map[String, String])
+
+final case class AppStatus(status: true, requestMetadata: Metadata)
 
 object AppStatus {
-  val Up: AppStatus = AppStatus(true)
+  def up(metadata: Metadata): AppStatus = AppStatus(true, metadata)
+
+  def up(request: ServerRequest): AppStatus =
+    up {
+      Metadata(
+        request.uri.toString(),
+        request.headers.map(h => (h.name, h.value)).toMap
+      )
+    }
 }
 
 private[controllers] class HealthController[F[_]: Async] extends Controller[F] {
@@ -21,9 +32,10 @@ private[controllers] class HealthController[F[_]: Async] extends Controller[F] {
 
   private val statusEndpoint: ServerEndpoint[Fs2Streams[F], F] =
     infallibleEndpoint.get
+      .in(extractFromRequest(identity))
       .in("health" / "status")
       .out(jsonBody[AppStatus])
-      .serverLogicPure(_ => AppStatus.Up.asRight[Nothing])
+      .serverLogicPure(req => AppStatus.up(req).asRight[Nothing])
 
   override def routes: HttpRoutes[F] = Http4sServerInterpreter[F]().toRoutes(statusEndpoint)
 
--- core/src/test/scala/ebayapp/core/controllers/HealthControllerSpec.scala
+++ core/src/test/scala/ebayapp/core/controllers/HealthControllerSpec.scala
@@ -2,8 +2,10 @@ package ebayapp.core.controllers
 
 import cats.effect.IO
 import ebayapp.core.ControllerSpec
+import org.http4s.Header.Raw
 import org.http4s.implicits._
 import org.http4s._
+import org.typelevel.ci.CIString
 
 class HealthControllerSpec extends ControllerSpec {
 
@@ -12,10 +14,11 @@ class HealthControllerSpec extends ControllerSpec {
     "return status on the app" in {
       val controller = new HealthController[IO]
 
-      val request  = Request[IO](uri = uri"/health/status", method = Method.GET)
+      val request  = Request[IO](uri = uri"/health/status", method = Method.GET, headers = Headers(Raw(CIString("foo"), "bar")))
       val response = controller.routes.orNotFound.run(request)
 
-      verifyJsonResponse(response, Status.Ok, Some("""{"status": true}"""))
+      val responseBody = """{"status":true,"requestMetadata":{"uri":"/health/status","headers":{"foo":"bar"}}}"""
+      verifyJsonResponse(response, Status.Ok, Some(responseBody))
     }
   }
 }
