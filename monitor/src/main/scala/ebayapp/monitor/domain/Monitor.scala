c52f503328adc9eef77c466b3dbda21d3eb28534
kirill5k <immotional@aol.com>
2021-12-20 17:15:53 +0000

    Monitor domain

---
+++ monitor/src/main/scala/ebayapp/monitor/domain/Monitor.scala
@@ -0,0 +1,30 @@
+package ebayapp.monitor.domain
+
+import java.util.UUID
+import java.net.URL
+import scala.concurrent.duration.FiniteDuration
+
+final case class Monitor(
+    id: Monitor.Id,
+    name: Monitor.Name,
+    url: URL,
+    interval: FiniteDuration,
+    timeout: FiniteDuration,
+    notification: Monitor.Notification
+)
+
+object Monitor {
+  opaque type Id = String
+  object Id {
+    def gen: Id = UUID.randomUUID().toString
+  }
+
+  opaque type Name = String
+  object Name {
+    def apply(name: String): Name = name
+  }
+
+  enum Notification {
+    case Email(email: String)
+  }
+}
