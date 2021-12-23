73b56ad5d14bd28bd53291d632491900ae16f322
kirill5k <immotional@aol.com>
2021-12-23 20:15:28 +0000

    ActionProcessor

--- monitor/src/main/scala/ebayapp/monitor/actions/ActionProcessor.scala
+++ monitor/src/main/scala/ebayapp/monitor/actions/ActionProcessor.scala
@@ -1,7 +1,20 @@
 package ebayapp.monitor.actions
 
+import cats.Monad
+import cats.effect.Temporal
 import cats.effect.std.Queue
+import fs2.Stream
 
-final class ActionProcessor[F[_]](
+trait ActionProcessor[F[_]]:
+  def run: Stream[F, Unit]
+
+final private class LiveActionProcessor[F[_]: Temporal](
     val actions: Queue[F, Action]
-)
+) extends ActionProcessor[F] {
+
+  def run: Stream[F, Unit] = Stream.fromQueueUnterminated(actions).drain
+}
+
+object ActionProcessor:
+  def make[F[_]: Temporal](actions: Queue[F, Action]): F[ActionProcessor[F]] =
+    Monad[F].pure(LiveActionProcessor(actions))
