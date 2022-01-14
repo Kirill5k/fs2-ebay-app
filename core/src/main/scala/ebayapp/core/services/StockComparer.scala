cc78445aad8e1a05b02dc78b0a1dafe7c96279b0
kirill5k <immotional@aol.com>
2022-01-14 17:40:12 +0000

    Refactor StockComparer again

--- core/src/main/scala/ebayapp/core/services/StockComparer.scala
+++ core/src/main/scala/ebayapp/core/services/StockComparer.scala
@@ -13,13 +13,15 @@ object StockComparer:
       curr: Map[String, ResellableItem],
       req: StockMonitorRequest
   ): List[ItemStockUpdates] =
-    curr.flatMap { (name, currItem) =>
-      val updates = prev
-        .get(name)
-        .fold(List[StockUpdate](StockUpdate.New)) { prevItem =>
-          val upd1 = req.monitorPriceChange.guard[Option] >> StockUpdate.priceChanged(prevItem.buyPrice, currItem.buyPrice)
-          val upd2 = req.monitorStockChange.guard[Option] >> StockUpdate.quantityChanged(prevItem.buyPrice, currItem.buyPrice)
-          upd1.toList ++ upd2.toList
-        }
-      updates.nonEmpty.guard[Option].as(ItemStockUpdates(currItem, updates))
-    }.toList
+    curr
+      .map((name, currItem) => (prev.get(name), currItem))
+      .flatMap {
+        case (None, currItem) =>
+          Some(ItemStockUpdates(currItem, List(StockUpdate.New)))
+        case (Some(prevItem), currItem) =>
+          val upd1    = req.monitorPriceChange.guard[Option] >> StockUpdate.priceChanged(prevItem.buyPrice, currItem.buyPrice)
+          val upd2    = req.monitorStockChange.guard[Option] >> StockUpdate.quantityChanged(prevItem.buyPrice, currItem.buyPrice)
+          val updates = upd1.toList ++ upd2.toList
+          updates.nonEmpty.guard[Option].as(ItemStockUpdates(currItem, updates))
+      }
+      .toList
