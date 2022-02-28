87b7c0754e72487ea2960b2e7fe2bf7f76dca82a
kirill5k <immotional@aol.com>
2022-02-28 13:00:01 +0000

    SearchClient::searchWithFiltersApplied

--- core/src/main/scala/ebayapp/core/clients/SearchClient.scala
+++ core/src/main/scala/ebayapp/core/clients/SearchClient.scala
@@ -11,22 +11,23 @@ final case class SearchCriteria(
     category: Option[String] = None,
     itemKind: Option[ItemKind] = None,
     minDiscount: Option[Int] = None,
-    excludeFilters: Option[List[String]] = None
+    excludeFilters: Option[List[String]] = None,
+    includeFilters: Option[List[String]] = None
 ) derives ConfigReader:
-  val excludeFilterRegex: Option[String] = excludeFilters.map(_.mkString("(?i).*(", "|", ").*"))
-
+  val excludeFilterRegex: Option[String]  = excludeFilters.map(_.mkString("(?i).*(", "|", ").*"))
+  val includeFiltersRegex: Option[String] = includeFilters.map(_.mkString("(?i).*(", "|", ").*"))
 
 enum Retailer(val name: String):
-  case Cex extends Retailer("cex")
-  case Ebay extends Retailer("ebay")
-  case Selfridges extends Retailer("selfridges")
-  case Argos extends Retailer("argos")
-  case Scotts extends Retailer("scotts")
-  case Jdsports extends Retailer("jdsports")
-  case Tessuti extends Retailer("tessuti")
-  case Nvidia extends Retailer("nvidia")
-  case Scan extends Retailer("scan")
-  case HarveyNichols extends Retailer("harvey-nichols")
+  case Cex              extends Retailer("cex")
+  case Ebay             extends Retailer("ebay")
+  case Selfridges       extends Retailer("selfridges")
+  case Argos            extends Retailer("argos")
+  case Scotts           extends Retailer("scotts")
+  case Jdsports         extends Retailer("jdsports")
+  case Tessuti          extends Retailer("tessuti")
+  case Nvidia           extends Retailer("nvidia")
+  case Scan             extends Retailer("scan")
+  case HarveyNichols    extends Retailer("harvey-nichols")
   case MainlineMenswear extends Retailer("mainline-menswear")
 
 object Retailer:
@@ -35,3 +36,15 @@ object Retailer:
 
 trait SearchClient[F[_]]:
   def search(criteria: SearchCriteria): Stream[F, ResellableItem]
+
+  def searchWithFiltersApplied(criteria: SearchCriteria): Stream[F, ResellableItem] =
+    search(criteria)
+      .filter(item => criteria.minDiscount.fold(true)(min => item.buyPrice.discount.exists(_ >= min)))
+      .filter { ri =>
+        ri.itemDetails.fullName match {
+          case Some(name) =>
+            criteria.excludeFilterRegex.fold(true)(filter => !name.matches(filter)) &&
+              criteria.includeFilters.fold(true)(filter => name.matches(filter))
+          case None => true
+        }
+      }
