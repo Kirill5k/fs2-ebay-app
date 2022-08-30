be22231579157d3657998c2443e68335a706e13f
kirill5k <immotional@aol.com>
2022-08-30 15:00:47 +0100

    Revert "Get product before getting stock"
    This reverts commit 81b9953fac4337f9beda6d0e6d3ccfb116ec6961.

--- core/src/test/scala/ebayapp/core/clients/jdsports/JdsportsClientSpec.scala
+++ core/src/test/scala/ebayapp/core/clients/jdsports/JdsportsClientSpec.scala
@@ -30,12 +30,8 @@ class JdsportsClientSpec extends SttpClientSpec {
             Response("n/a", StatusCode.NotFound)
           case r if r.isGoingTo("jdsports.com/proxy/product/black-emporio-armani-ea7-tape-2-t-shirt/16022719/stock/") =>
             Response.ok(json("jdsports/get-product-stock.html"))
-          case r if r.isGoingTo("jdsports.com/proxy/product/black-emporio-armani-ea7-tape-2-t-shirt/16022719/") =>
-            Response.ok("ok")
           case r if r.isGoingTo("jdsports.com/proxy/product/black-emporio-armani-ea7-padded-zip-bubble-jacket/16026576/stock/") =>
             Response.ok(json("jdsports/get-product-stock-oos.html"))
-          case r if r.isGoingTo("jdsports.com/proxy/product/black-emporio-armani-ea7-padded-zip-bubble-jacket/16026576/") =>
-            Response.ok("ok")
           case r => throw new RuntimeException(r.uri.toString())
         }
 
