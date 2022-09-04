581d18e8c12c9968f7dc90b55526f389a2eafcdf
kirill5k <immotional@aol.com>
2022-09-04 15:30:35 +0100

    Clean EbayClient

--- core/src/main/scala/ebayapp/core/clients/ebay/EbayClient.scala
+++ core/src/main/scala/ebayapp/core/clients/ebay/EbayClient.scala
@@ -10,7 +10,8 @@ import ebayapp.core.clients.ebay.browse.EbayBrowseClient
 import ebayapp.core.clients.ebay.browse.responses.{EbayItem, EbayItemSummary}
 import ebayapp.core.clients.ebay.mappers.EbayItemMapper
 import ebayapp.core.clients.ebay.search.EbaySearchParams
-import ebayapp.core.common.config.EbayConfig
+import ebayapp.core.common.config.{EbayConfig, EbaySearchConfig}
+import ebayapp.core.common.predicates.*
 import ebayapp.kernel.errors.AppError
 import ebayapp.core.domain.ResellableItem
 import ebayapp.core.domain.search.SearchCriteria
@@ -19,7 +20,6 @@ import fs2.Stream
 import sttp.client3.SttpBackend
 
 import java.time.Instant
-import java.time.temporal.ChronoField.MILLI_OF_SECOND
 
 final private[ebay] class LiveEbayClient[F[_]](
     private val config: EbayConfig,
@@ -33,33 +33,33 @@ final private[ebay] class LiveEbayClient[F[_]](
   private def now: F[Instant] = F.realTime.map(_.toMillis).map(Instant.ofEpochMilli)
 
   def search(criteria: SearchCriteria): Stream[F, ResellableItem] =
-    for {
+    for
       time   <- Stream.eval(now.map(_.minusMillis(config.search.maxListingDuration.toMillis)))
       mapper <- Stream.fromEither[F](EbayItemMapper.get(criteria))
       params <- Stream.fromEither[F](EbaySearchParams.get(criteria))
       items <- Stream
-        .evalSeq(searchForItems(params.requestArgs(time, criteria.query), params.filter))
+        .evalSeq(searchForItems(params.requestArgs(time, criteria.query), params.filter and hasTrustedSeller(config.search)))
         .evalMap(getCompleteItem)
         .unNone
         .map(mapper.toDomain(criteria))
         .handleErrorWith(switchAccountIfItHasExpired)
-    } yield items
+    yield items
 
   private def searchForItems(searchParams: Map[String, String], itemsFilter: EbayItemSummary => Boolean): F[List[EbayItemSummary]] =
-    for {
+    for
       token <- authClient.accessToken
       all   <- browseClient.search(token, searchParams)
-      valid = all.filter(_.itemGroupType.isEmpty).filter(hasTrustedSeller).filter(itemsFilter)
+      valid = all.filter(_.itemGroupType.isEmpty).filter(itemsFilter)
       _ <- logger.info(s"""ebay-search "${searchParams("q")}" returned ${valid.size} new items (total - ${all.size})""")
-    } yield valid
+    yield valid
 
   private def getCompleteItem(itemSummary: EbayItemSummary): F[Option[EbayItem]] =
     authClient.accessToken.flatMap(t => browseClient.getItem(t, itemSummary.itemId))
 
-  private val hasTrustedSeller: EbayItemSummary => Boolean = is =>
+  private def hasTrustedSeller(searchConfig: EbaySearchConfig): EbayItemSummary => Boolean = is =>
     (is.seller.feedbackPercentage, is.seller.feedbackScore)
       .mapN { (percentage, score) =>
-        percentage > config.search.minFeedbackPercentage && score > config.search.minFeedbackScore
+        percentage > searchConfig.minFeedbackPercentage && score > searchConfig.minFeedbackScore
       }
       .exists(identity)
 
