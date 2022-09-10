0d69a5a00c35257ad55cab6b27448a9312cfe4d1
kirill5k <immotional@aol.com>
2022-09-10 09:03:06 +0100

    Clean ConfigProvider

--- core/src/main/scala/ebayapp/core/common/ConfigProvider.scala
+++ core/src/main/scala/ebayapp/core/common/ConfigProvider.scala
@@ -1,5 +1,6 @@
 package ebayapp.core.common
 
+import cats.Monad
 import cats.effect.{Async, Ref, Sync}
 import cats.effect.syntax.spawn.*
 import cats.syntax.flatMap.*
@@ -18,27 +19,27 @@ trait ConfigProvider[F[_]]:
 final private class LiveConfigProvider[F[_]](
     private val state: Ref[F, AppConfig]
 )(using
-    F: Async[F]
+    F: Monad[F]
 ) extends ConfigProvider[F] {
   override def config: F[AppConfig] = state.get
 }
 
-object ConfigProvider {
+object ConfigProvider:
 
   def make[F[_]](reloadEvery: FiniteDuration)(using F: Async[F], logger: Logger[F]): F[ConfigProvider[F]] = {
-    def configReloadProcess(state: Ref[F, AppConfig]): F[Unit] =
+    def reloadUpdatedConfig(state: Ref[F, AppConfig]): F[Unit] =
       F.sleep(reloadEvery) *>
-      (mountedConfigModifiedDate, F.realTimeInstant).mapN { (modifiedDate, now) =>
-        F.whenA(modifiedDate.plusSeconds(reloadEvery.toSeconds + 30L).isAfter(now)) {
-          F.blocking(AppConfig.loadFromMount).flatMap(c => logger.info("reloading updated config from mount") *> state.set(c))
-        }
-      }.flatten *>
-        configReloadProcess(state)
+        (mountedConfigModifiedDate, F.realTimeInstant).mapN { (modifiedDate, now) =>
+          F.whenA(modifiedDate.plusSeconds(reloadEvery.toSeconds + 30L).isAfter(now)) {
+            F.blocking(AppConfig.loadFromMount).flatMap(c => logger.info("reloading updated config from mount") *> state.set(c))
+          }
+        }.flatten *>
+        reloadUpdatedConfig(state)
 
     F
       .blocking(AppConfig.loadFromMount)
       .flatMap(c => logger.info("loaded config from a configmap mount") *> Ref.of(c))
-      .flatTap(s => configReloadProcess(s).start.void)
+      .flatTap(s => reloadUpdatedConfig(s).start.void)
       .handleErrorWith { e =>
         logger.warn(s"error loading config from a configmap mount, will try resources: ${e.getMessage}") *>
           F.blocking(AppConfig.loadDefault).flatMap(c => Ref.of(c))
@@ -48,4 +49,3 @@ object ConfigProvider {
 
   private def mountedConfigModifiedDate[F[_]](using F: Sync[F]): F[Instant] =
     F.delay(Paths.get(AppConfig.mountedConfigPath).toFile.lastModified()).map(Instant.ofEpochMilli)
-}
