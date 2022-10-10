254028a99973781a0c3a1b745d7e1061dea4afda
kirill5k <immotional@aol.com>
2022-10-10 09:16:58 +0100

    Test for new config

--- core/src/test/scala/ebayapp/core/common/AppConfigSpec.scala
+++ core/src/test/scala/ebayapp/core/common/AppConfigSpec.scala
@@ -16,18 +16,13 @@ class AppConfigSpec extends CatsSpec {
       conf.mongo.dbName mustBe "ebay-app"
       conf.server.host mustBe "0.0.0.0"
       conf.retailer.cex.baseUri mustBe "https://wss2.cex.uk.webuy.io"
-      conf.retailer.selfridges.headers mustBe Map("api-key" -> "key")
-      conf.retailer.nvidia.proxied mustBe Some(true)
-      conf.retailer.harveyNichols.baseUri mustBe "https://www.harveynichols.com"
+      conf.retailer.selfridges.headers mustBe Map("X-Reroute-To" -> "https://www.selfridges.com", "User-Agent" -> "PostmanRuntime/7.28.3", "referer" -> "https://www.selfridges.com", "api-key" -> "key")
+      conf.retailer.argos.proxied mustBe Some(true)
       conf.retailer.jdsports.delayBetweenIndividualRequests mustBe Some(2.seconds)
 
-      val geforceFilters = Some(Filters(None, Some(List("GTX 1650")), None))
-      val geforceMonReq  = StockMonitorRequest(SearchCriteria("geforce", Some("GPU"), filters = geforceFilters), true, true)
-      conf.stockMonitor must contain(Retailer.Nvidia -> StockMonitorConfig(5.minutes, List(geforceMonReq)))
-
-      val ps5Filters = Some(Filters(Some(20), None, None))
-      val ps5MonReq  = StockMonitorRequest(SearchCriteria("PlayStation 5 Console", None), false, true)
-      conf.stockMonitor must contain(Retailer.Argos -> StockMonitorConfig(10.minutes, List(ps5MonReq), None, ps5Filters))
+      val geforceFilters = Some(Filters(None, Some(List("GTX 1650", "GTX 1660")), None))
+      val geforceMonReq  = StockMonitorRequest(SearchCriteria("geforce", Some("GPU")), true, true)
+      conf.stockMonitor must contain(Retailer.Nvidia -> StockMonitorConfig(10.minutes, List(geforceMonReq), filters = geforceFilters))
 
       conf.stockMonitor must contain key Retailer.HarveyNichols
       conf.dealsFinder must contain key Retailer.Ebay
