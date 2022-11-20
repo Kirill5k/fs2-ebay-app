6e2ddfcb8527774eb84b95976a30c41622311fec
kirill5k <immotional@aol.com>
2022-11-20 09:13:58 +0000

    Change logging level in JD client

--- core/src/main/scala/ebayapp/core/clients/jdsports/JdsportsClient.scala
+++ core/src/main/scala/ebayapp/core/clients/jdsports/JdsportsClient.scala
@@ -113,7 +113,7 @@ final private class LiveJdsportsClient[F[_]](
           case Right(html) =>
             F.fromEither(ResponseParser.parseSearchResponse(html))
           case Left(_) if r.code == StatusCode.Forbidden =>
-            logger.warn(s"$name-search/403") *> F.sleep(10.seconds) *> searchByBrand(criteria, step)
+            logger.error(s"$name-search/403") *> F.sleep(10.seconds) *> searchByBrand(criteria, step)
           case Left(_) if r.code == StatusCode.NotFound && step == 0 =>
             logger.warn(s"$name-search/404 - ${r.request.uri.toString()}") *> F.pure(Nil)
           case Left(_) if r.code == StatusCode.NotFound =>
