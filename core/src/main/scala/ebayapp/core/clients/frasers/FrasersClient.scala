42ae36884b3f15cf67f633ca2b99ebfd6b4f7d5d
kirill5k <immotional@aol.com>
2023-04-13 14:29:48 +0100

    Generalise flannels client

--- core/src/main/scala/ebayapp/core/clients/frasers/FrasersClient.scala
+++ core/src/main/scala/ebayapp/core/clients/frasers/FrasersClient.scala
@@ -4,12 +4,12 @@ import cats.Monad
 import cats.effect.Temporal
 import cats.syntax.flatMap.*
 import cats.syntax.functor.*
-import ebayapp.core.clients.frasers.mappers.{FlannelsItem, flannelsClothingMapper}
+import ebayapp.core.clients.frasers.mappers.{flannelsClothingMapper, FlannelsItem}
 import ebayapp.core.clients.frasers.responses.{FlannelsProduct, FlannelsSearchResponse}
 import ebayapp.core.clients.{HttpClient, SearchClient}
 import ebayapp.core.common.config.GenericRetailerConfig
 import ebayapp.core.common.{ConfigProvider, Logger}
-import ebayapp.core.domain.ResellableItem
+import ebayapp.core.domain.{ResellableItem, Retailer}
 import ebayapp.core.domain.search.SearchCriteria
 import fs2.Stream
 import sttp.client3.*
@@ -19,16 +19,23 @@ import sttp.model.{Header, MediaType, StatusCode, Uri}
 
 import scala.concurrent.duration.*
 
-final private class LiveFlannelsClient[F[_]](
+final private class LiveFrasersClient[F[_]](
     private val configProvider: () => F[GenericRetailerConfig],
     override val httpBackend: SttpBackend[F, Any],
-    override val proxyBackend: Option[SttpBackend[F, Any]]
+    override val proxyBackend: Option[SttpBackend[F, Any]],
+    private val retailer: Retailer.Flannels.type | Retailer.Tessuti.type
 )(using
     F: Temporal[F],
     logger: Logger[F]
 ) extends SearchClient[F] with HttpClient[F] {
 
-  override protected val name: String = "flannels"
+  override protected val name: String = retailer.name
+
+  extension (c: GenericRetailerConfig) def websiteUri = c.headers.getOrElse("X-Reroute-To", c.baseUri) + "/"
+
+  val groupIdPrefix: String = retailer match
+    case Retailer.Flannels => "FLAN_TM"
+    case Retailer.Tessuti  => "TESS_BR"
 
   override def search(criteria: SearchCriteria): Stream[F, ResellableItem] =
     Stream
@@ -49,13 +56,13 @@ final private class LiveFlannelsClient[F[_]](
     configProvider()
       .flatMap { config =>
         val args = Map(
-          "categoryId" -> s"FLAN_TM${sc.query.toUpperCase.replaceAll("[ -]", "")}",
-          "page" -> page.toString,
-          "productsPerPage" -> "100",
-          "sortOption" -> "discountvalue_desc",
-          "isSearch" -> "false",
-          "clearFilters" -> "false",
-          "pathName" -> s"/${sc.query.replaceAll(" ", "-")}${sc.category.fold("")(c => s"/$c")}",
+          "categoryId"       -> s"${groupIdPrefix}${sc.query.toUpperCase.replaceAll("[ -]", "")}",
+          "page"             -> page.toString,
+          "productsPerPage"  -> "100",
+          "sortOption"       -> "discountvalue_desc",
+          "isSearch"         -> "false",
+          "clearFilters"     -> "false",
+          "pathName"         -> s"/${sc.query.replaceAll(" ", "-")}${sc.category.fold("")(c => s"/$c")}",
           "selectedCurrency" -> "GBP"
         )
         dispatchWithProxy(config.proxied) {
@@ -66,7 +73,7 @@ final private class LiveFlannelsClient[F[_]](
             .header(Header.userAgent(operaUserAgent))
             .header(Header.cacheControl(CacheDirective.NoCache))
             .header("accept-language", "en-GB,en-US")
-            .header("referer", "https://www.flannels.com")
+            .header("referer", config.websiteUri)
             .headers(config.headers)
             .get(uri"${config.baseUri}/api/productlist/v1/getforcategory?${args}")
             .response(asJson[FlannelsSearchResponse])
@@ -75,7 +82,7 @@ final private class LiveFlannelsClient[F[_]](
       .flatMap { r =>
         r.body match
           case Right(res) =>
-            F.pure(res.products -> Option.when(res.products.nonEmpty)(page+1))
+            F.pure(res.products -> Option.when(res.products.nonEmpty)(page + 1))
           case Left(DeserializationException(_, error)) if error.getMessage.contains("exhausted input") =>
             logger.warn(s"$name-search/exhausted input") >>
               F.sleep(3.second) >> getItems(sc)(page)
@@ -97,4 +104,4 @@ object FrasersClient:
       backend: SttpBackend[F, Any],
       proxyBackend: Option[SttpBackend[F, Any]] = None
   ): F[SearchClient[F]] =
-    Monad[F].pure(LiveFlannelsClient[F](() => configProvider.flannels, backend, proxyBackend))
+    Monad[F].pure(LiveFrasersClient[F](() => configProvider.flannels, backend, proxyBackend, Retailer.Flannels))
