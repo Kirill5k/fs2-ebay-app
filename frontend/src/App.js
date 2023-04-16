6a06bd7bafc0d2b4804009424be2ddb9995e96ca
kirill5k <immotional@aol.com>
2023-04-16 13:29:21 +0100

    Setup antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { App as AntdApp, Button, Space, ConfigProvider, theme } from 'antd';
 import './App.css';
+
+const MyPage = () => {
+  const { message, notification, modal } = AntdApp.useApp();
+  const showMessage = () => {
+    message.success('Success!');
+  };
+
+  const showModal = () => {
+    modal.warning({
+      title: 'This is a warning message',
+      content: 'some messages...some messages...',
+    });
+  };
+
+  const showNotification = () => {
+    notification.info({
+      message: `Notification topLeft`,
+      description: 'Hello, Ant Design!!',
+      placement: 'topLeft',
+    });
+  };
+
+  return (
+      <Space>
+        <Button type="primary" onClick={showMessage}>
+          Open message
+        </Button>
+        <Button type="primary" onClick={showModal}>
+          Open modal
+        </Button>
+        <Button type="primary" onClick={showNotification}>
+          Open notification
+        </Button>
+      </Space>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ConfigProvider theme={theme.compactAlgorithm} >
+        <AntdApp>
+          <MyPage />
+        </AntdApp>
+      </ConfigProvider>
   );
 }
 
