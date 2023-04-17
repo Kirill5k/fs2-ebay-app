8a5c3c9e186bec2a282bbe3fbf4e0580b2befbf9
kirill5k <immotional@aol.com>
2023-04-17 14:30:51 +0100

    Remove breadcrumb from main

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,4 +1,4 @@
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Layout, Menu, theme } from "antd";
 import React from "react";
 import {
   LaptopOutlined,
@@ -34,25 +34,18 @@ const Home = () => {
   } = theme.useToken();
 
   return (
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
-          <Sider style={{ background: colorBgContainer }} width={200}>
-            <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-                items={menuItems}
-            />
-          </Sider>
-          <Content style={{ padding: '0 24px', minHeight: 280 }}>Home page content</Content>
-        </Layout>
-      </Content>
+      <Layout style={{ padding: '24px 0', margin: '16px 50px', background: colorBgContainer }}>
+        <Sider style={{ background: colorBgContainer }} width={200}>
+          <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+              items={menuItems}
+          />
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>Home page content</Content>
+      </Layout>
   );
 }
 
