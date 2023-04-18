e7491b00da0300b400fc904707d549d42870be9d
kirill5k <immotional@aol.com>
2023-04-18 10:12:41 +0100

    Set default select key to 0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
         <Layout>
           <Layout.Header className="header">
             <div className="logo" />
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['0']}>
               <Menu.Item key="0" title="Home">
                 <Link to="/">Home</Link>
               </Menu.Item>
