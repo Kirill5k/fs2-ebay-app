bcb279e0cb826add62e365d93a6e633b39a19cc1
kirill5k <immotional@aol.com>
2023-04-22 10:05:16 +0100

    Add dayjs

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import dayjs from 'dayjs'
 import {DatePicker} from 'antd'
 import Container from '../common/components/Container'
 
 const Home = ({backgroundColor}) => {
+
+  const now = dayjs()
+  const startOfDay = now.startOf('day')
+  const endOfDay = now.endOf('day')
+
   return (
       <Container
           column
           style={{
             padding: '24px 24px',
-            background: backgroundColor,
+            background: backgroundColor
           }}>
-        <DatePicker.RangePicker showTime/>
+        <DatePicker.RangePicker
+            defaultValue={[startOfDay, endOfDay]}
+            onChange={(v) => console.log(v)}
+            showTime
+        />
         <p>Home page content</p>
       </Container>
   )
