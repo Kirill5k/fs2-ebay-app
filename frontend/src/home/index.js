fc90653af3ac4264a2fb627dddb347a82932b3ba
kirill5k <immotional@aol.com>
2023-04-25 11:26:38 +0100

    Display data on home page

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+import {Descriptions} from 'antd'
 import Container from '../common/components/Container'
 
 const Home = ({backgroundColor}) => {
+  const stockStatus = useSelector(state => state.stock.status)
+  const stockItems = useSelector(state => state.stock.items)
+  const stockRetailers = useSelector(state => state.stock.filters.retailers)
+  const dealsStatus = useSelector(state => state.deals.status)
+  const dealsItems = useSelector(state => state.deals.todayItems)
+
   return (
       <Container
           column
@@ -9,7 +17,19 @@ const Home = ({backgroundColor}) => {
             padding: '24px 24px',
             background: backgroundColor
           }}>
-        <p>Home page content</p>
+        { dealsStatus === 'succeeded' && (
+            <Descriptions title="Deals">
+              <Descriptions.Item label="Total for today">{dealsItems.total}</Descriptions.Item>
+              <Descriptions.Item label="Without exchange price">{dealsItems.unrecognized.total}</Descriptions.Item>
+              <Descriptions.Item label="With sell price smaller than exchange">{dealsItems.profitable.total}</Descriptions.Item>
+            </Descriptions>
+        )}
+        { stockStatus === 'succeeded' && (
+            <Descriptions title="Stock">
+              <Descriptions.Item label="Total">{stockItems.length}</Descriptions.Item>
+              <Descriptions.Item label="Tracked retailers">{stockRetailers.join(', ')}</Descriptions.Item>
+            </Descriptions>
+        )}
       </Container>
   )
 }
