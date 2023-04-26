8920970f42f84e86076c2d28dd921f9e667a6c37
kirill5k <immotional@aol.com>
2023-04-26 11:11:27 +0100

    Create component for deals

---
+++ frontend/src/deals/DealsItems.js
@@ -0,0 +1,36 @@
+import InfiniteScroll from 'react-infinite-scroll-component'
+import {Card, Descriptions, List} from 'antd'
+
+const DealsItems = ({items}) => (
+    <InfiniteScroll
+        dataLength={items.total}
+        hasMore={false}
+        loader={<h4>Loading...</h4>}
+        endMessage={<span></span>}
+        height={300}
+    >
+      <List
+          dataSource={items.items}
+          renderItem={(item) => (
+              <Card key={item.url} size="small">
+                <Descriptions
+                    size="small"
+                    column={1}
+                >
+                  <Descriptions.Item label="Name">
+                    {item.name}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Listing Title">
+                    {item.title}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Price">
+                    {item.buyPrice} (Buy) {item.exchangePrice ? `/ ${item.exchangePrice} (Sell)` : ''}
+                  </Descriptions.Item>
+                </Descriptions>
+              </Card>
+          )}
+      />
+    </InfiniteScroll>
+)
+
+export default DealsItems
--- frontend/src/deals/index.js
+++ frontend/src/deals/index.js
@@ -1,41 +1,10 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
-import {DatePicker, Descriptions, List, Card} from 'antd'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import {DatePicker} from 'antd'
 import Container from '../common/components/Container'
 import {endOfToday, startOfToday} from '../common/functions/dates'
+import DealsItems from './DealsItems'
 
-const ItemsList = ({items}) => (
-    <InfiniteScroll
-        dataLength={items.total}
-        hasMore={false}
-        loader={<h4>Loading...</h4>}
-        endMessage={<span></span>}
-        height={300}
-    >
-      <List
-          dataSource={items.items}
-          renderItem={(item) => (
-              <Card key={item.url} size="small">
-                <Descriptions
-                    size="small"
-                    column={1}
-                >
-                  <Descriptions.Item label="Name">
-                    {item.name}
-                  </Descriptions.Item>
-                  <Descriptions.Item label="Listing Title">
-                    {item.title}
-                  </Descriptions.Item>
-                  <Descriptions.Item label="Price">
-                    {item.buyPrice} (Buy) {item.exchangePrice ? `/ ${item.exchangePrice} (Sell)` : ''}
-                  </Descriptions.Item>
-                </Descriptions>
-              </Card>
-          )}
-      />
-    </InfiniteScroll>
-)
 
 const Deals = ({backgroundColor}) => {
 
@@ -53,7 +22,7 @@ const Deals = ({backgroundColor}) => {
             onChange={(v) => console.log(v.map(d => d.toDate().toISOString()))}
             showTime
         />
-        <ItemsList items={items.unrecognized}/>
+        <DealsItems items={items.unrecognized}/>
       </Container>
   )
 }
