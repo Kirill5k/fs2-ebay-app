f3f3541278f45590d4862897a9a157e5f8a5c972
kirill5k <immotional@aol.com>
2023-05-20 10:30:53 +0100

    Count items by property

--- frontend/src/common/functions/collections.js
+++ frontend/src/common/functions/collections.js
@@ -1,18 +1,18 @@
 export const distinct = (array) => {
-  const newArray = [];
+  const newArray = []
   for (let i = 0; i < array.length; i++) {
     if (array[i]) {
-      newArray.push(array[i]);
+      newArray.push(array[i])
     }
   }
-  return [...new Set(newArray)];
+  return [...new Set(newArray)]
 }
 
 export const countByProperty = (array, propertyExtractor) => {
   return array.reduce((acc, item) => {
-    const prop = propertyExtractor(item);
-    const count = acc[`${prop}`] || 0;
-    acc[`${prop}`] = count + 1;
-    return acc;
-  }, {});
+    const prop = propertyExtractor(item)
+    const count = acc[`${prop}`] || 0
+    acc[`${prop}`] = count + 1
+    return acc
+  }, {})
 }
