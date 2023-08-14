d791ec582ce9af61909546d1ed40bd841f82c47d
kirill5k <immotional@aol.com>
2023-08-14 15:28:01 +0100

    Move MonitorController endpoint definitions to companion object

--- monitor/src/main/scala/ebayapp/monitor/controllers/MonitorController.scala
+++ monitor/src/main/scala/ebayapp/monitor/controllers/MonitorController.scala
@@ -5,19 +5,15 @@ import cats.effect.Async
 import cats.syntax.flatMap.*
 import ebayapp.kernel.errors.AppError
 import ebayapp.kernel.controllers.Controller
-import ebayapp.monitor.controllers.views.{
-  ActivateMonitorRequest,
-  CreateMonitorRequest,
-  CreateMonitorResponse,
-  MonitorView,
-  MonitoringEventView
-}
+import ebayapp.monitor.controllers.views.{ActivateMonitorRequest, CreateMonitorRequest, CreateMonitorResponse, MonitorView, MonitoringEventView}
 import ebayapp.monitor.domain.{HttpMethod, Monitor, Url}
 import ebayapp.monitor.services.{MonitorService, MonitoringEventService}
 import org.bson.types.ObjectId
 import org.http4s.HttpRoutes
 import sttp.model.StatusCode
 import sttp.tapir.*
+import sttp.tapir.generic.auto.SchemaDerivation
+import sttp.tapir.json.circe.TapirJsonCirce
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 
 import scala.concurrent.duration.FiniteDuration
@@ -29,32 +25,15 @@ final private class LiveMonitorController[F[_]](
     F: Async[F]
 ) extends Controller[F]:
 
-  given Schema[HttpMethod]         = Schema.string
-  given Schema[Url]                = Schema.string
-  given Schema[FiniteDuration]     = Schema.string
-  given Schema[Monitor.Contact]    = Schema.string
-  given Schema[Monitor.Connection] = Schema.string
-  given Schema[Monitor.Status]     = Schema.string
-
-  private val basePath   = "monitors"
-  private val idPath     = basePath / path[String]
-  private val eventsPath = idPath / "events"
-
   private def parseId(id: String): F[Monitor.Id] =
     F.fromEither(Either.cond(ObjectId.isValid(id), Monitor.Id(id), AppError.Invalid(s"Monitor id $id is invalid")))
 
-  private val getAll = endpoint.get
-    .in(basePath)
-    .errorOut(Controller.errorResponse)
-    .out(jsonBody[List[MonitorView]])
+  private val getAll = MonitorController.getAll
     .serverLogic { _ =>
       monitorService.getAll.mapResponse(_.map(MonitorView.from))
     }
 
-  private val getById = endpoint.get
-    .in(idPath)
-    .errorOut(Controller.errorResponse)
-    .out(jsonBody[MonitorView])
+  private val getById = MonitorController.getById
     .serverLogic { id =>
       parseId(id)
         .flatMap(monitorService.find)
@@ -62,54 +41,35 @@ final private class LiveMonitorController[F[_]](
         .mapResponse(MonitorView.from)
     }
 
-  private val update = endpoint.put
-    .in(idPath)
-    .in(jsonBody[MonitorView])
-    .errorOut(Controller.errorResponse)
-    .out(statusCode(StatusCode.NoContent))
+  private val update = MonitorController.update
     .serverLogic { (id, mon) =>
       F.raiseWhen(id != mon.id)(AppError.Failed(s"Id in path is different from id in the request body"))
         .flatMap(_ => monitorService.update(mon.toDomain))
         .voidResponse
     }
 
-  private val createNew = endpoint.post
-    .in(basePath)
-    .in(jsonBody[CreateMonitorRequest])
-    .errorOut(Controller.errorResponse)
-    .out(statusCode(StatusCode.Created).and(jsonBody[CreateMonitorResponse]))
+  private val createNew = MonitorController.createNew
     .serverLogic { create =>
       monitorService
         .create(create.toDomain)
         .mapResponse(m => CreateMonitorResponse(m.id.value))
     }
 
-  private val activate = endpoint.put
-    .in(idPath / "active")
-    .in(jsonBody[ActivateMonitorRequest])
-    .errorOut(Controller.errorResponse)
-    .out(statusCode(StatusCode.NoContent))
+  private val activate = MonitorController.activate
     .serverLogic { (id, request) =>
       parseId(id)
         .flatMap(id => monitorService.activate(id, request.active))
         .voidResponse
     }
 
-  private val getEvents = endpoint.get
-    .in(eventsPath)
-    .in(query[Option[Int]]("limit"))
-    .errorOut(Controller.errorResponse)
-    .out(jsonBody[List[MonitoringEventView]])
+  private val getEvents = MonitorController.getEvents
     .serverLogic { (id, limit) =>
       parseId(id)
         .flatMap(mid => monitoringEventService.find(mid, limit.getOrElse(25)))
         .mapResponse(_.map(MonitoringEventView.from))
     }
 
-  private val delete = endpoint.delete
-    .in(idPath)
-    .errorOut(Controller.errorResponse)
-    .out(statusCode(StatusCode.NoContent))
+  private val delete = MonitorController.delete
     .serverLogic { id =>
       parseId(id)
         .flatMap(monitorService.delete)
@@ -119,6 +79,57 @@ final private class LiveMonitorController[F[_]](
   override def routes: HttpRoutes[F] =
     Http4sServerInterpreter[F](serverOptions).toRoutes(List(getAll, getById, createNew, getEvents, activate, update, delete))
 
-object MonitorController:
-  def make[F[_]: Async](monitorService: MonitorService[F], monitoringEventService: MonitoringEventService[F]): F[Controller[F]] =
-    Monad[F].pure(LiveMonitorController[F](monitorService, monitoringEventService))
+object MonitorController extends TapirJsonCirce with SchemaDerivation {
+  given Schema[HttpMethod] = Schema.string
+  given Schema[Url] = Schema.string
+  given Schema[FiniteDuration] = Schema.string
+  given Schema[Monitor.Contact] = Schema.string
+  given Schema[Monitor.Connection] = Schema.string
+  given Schema[Monitor.Status] = Schema.string
+
+  private val basePath = "monitors"
+  private val idPath = basePath / path[String]
+  private val eventsPath = idPath / "events"
+
+  val getAll = endpoint.get
+    .in(basePath)
+    .errorOut(Controller.errorResponse)
+    .out(jsonBody[List[MonitorView]])
+
+  val getById = endpoint.get
+    .in(idPath)
+    .errorOut(Controller.errorResponse)
+    .out(jsonBody[MonitorView])
+
+  val update = endpoint.put
+    .in(idPath)
+    .in(jsonBody[MonitorView])
+    .errorOut(Controller.errorResponse)
+    .out(statusCode(StatusCode.NoContent))
+
+  val createNew = endpoint.post
+    .in(basePath)
+    .in(jsonBody[CreateMonitorRequest])
+    .errorOut(Controller.errorResponse)
+    .out(statusCode(StatusCode.Created).and(jsonBody[CreateMonitorResponse]))
+
+  val activate = endpoint.put
+    .in(idPath / "active")
+    .in(jsonBody[ActivateMonitorRequest])
+    .errorOut(Controller.errorResponse)
+    .out(statusCode(StatusCode.NoContent))
+
+  val getEvents = endpoint.get
+    .in(eventsPath)
+    .in(query[Option[Int]]("limit"))
+    .errorOut(Controller.errorResponse)
+    .out(jsonBody[List[MonitoringEventView]])
+
+  val delete = endpoint.delete
+    .in(idPath)
+    .errorOut(Controller.errorResponse)
+    .out(statusCode(StatusCode.NoContent))
+  
+  def make[F[_]: Async](monitors: MonitorService[F], monitoringEvents: MonitoringEventService[F]): F[Controller[F]] =
+    Monad[F].pure(LiveMonitorController[F](monitors, monitoringEvents))
+}
