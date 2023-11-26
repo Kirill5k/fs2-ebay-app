9d9356f6e0aeaa99d83d38baf1b5a8051b9eda26
kirill5k <immotional@aol.com>
2023-11-26 13:55:18 +0000

    Clear warning on deals page

--- frontend/src/deals/DealsItems.js
+++ frontend/src/deals/DealsItems.js
@@ -1,34 +1,39 @@
 import {Card, Collapse, Descriptions, List, Tag} from 'antd'
 import './DealsItems.css'
 
-const DealsItemList = ({items}) => (
-    <div className="deals-item-list">
-      <List
-          dataSource={Array.from(items.items.entries())}
-          renderItem={([i, item]) => (
-              <Card
-                  key={item.url}
-                  className="deals-item"
-                  hoverable
-                  onClick={() => window.open(item.url, "_blank")}
-              >
-                <Descriptions column={1}>
-                  <Descriptions.Item label="Name">
-                    <span>{item.name}</span>
-                    <Tag bordered={false}>{i+1}</Tag>
-                  </Descriptions.Item>
-                  <Descriptions.Item label="Listing Title">
-                    {item.title}
-                  </Descriptions.Item>
-                  <Descriptions.Item label="Price">
-                    £{item.buyPrice} (Buy) {item.exchangePrice ? `/ £${item.exchangePrice} (Sell)` : ''}
-                  </Descriptions.Item>
-                </Descriptions>
-              </Card>
-          )}
-      />
-    </div>
+const ItemName = ({index, name}) => (
+    <span style={{width: '100%'}}>
+      <span>{name}</span>
+      <Tag bordered={false} style={{float: 'right'}}>{index}</Tag>
+    </span>
 )
+
+const DealsItemList = ({items}) => {
+
+  const itemDescription = (i, item) => [
+    { key: `name-${i}`, label: 'Name', children: <ItemName name={item.name} index={i}/> },
+    { key: `listing-title-${i}`, label: 'Listing Title', children: item.title },
+    { key: `price-${i}`, label: 'Price', children: `£${item.buyPrice} (Buy) ${item.exchangePrice ? `/ £${item.exchangePrice} (Sell)` : ''}` },
+  ]
+
+  return (
+      <div className="deals-item-list">
+        <List
+            dataSource={Array.from(items.items.entries())}
+            renderItem={([i, item]) => (
+                <Card
+                    key={item.url}
+                    className="deals-item"
+                    hoverable
+                    onClick={() => window.open(item.url, "_blank")}
+                >
+                  <Descriptions column={1} items={itemDescription(i, item)}/>
+                </Card>
+            )}
+        />
+      </div>
+  )
+}
 
 const DealsItems = ({items}) => (
     <Collapse
