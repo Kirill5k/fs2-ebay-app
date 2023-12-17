e08c7070d29b822eca4822b880b711fedf5efed9
kirill5k <immotional@aol.com>
2023-12-17 08:34:24 +0000

    Call new endpoint

--- frontend/src/deals/client.js
+++ frontend/src/deals/client.js
@@ -3,9 +3,9 @@ import { defaultRequestParams, reject } from '../common/functions/http'
 const today = () => new Date().toISOString().slice(0, 10)
 
 const DealsClient = {
-  getSummaries: ({from, to}) => fetch(`/api/video-games/summary?from=${from}&to=${to}`, defaultRequestParams)
+  getSummaries: ({from, to}) => fetch(`/api/resellable-items/summary?from=${from}&to=${to}`, defaultRequestParams)
       .then(res => res.status === 200 ? res.json() : reject(res)),
-  getSummariesForToday: () => fetch(`/api/video-games/summary?from=${today()}`, defaultRequestParams)
+  getSummariesForToday: () => fetch(`/api/resellable-items/summary?from=${today()}`, defaultRequestParams)
       .then(res => res.status === 200 ? res.json() : reject(res)),
 }
 
