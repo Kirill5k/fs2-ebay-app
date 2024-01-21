acaa1493359db346462df1f9c6a41cf3f08aad4b
kirill5k <immotional@aol.com>
2024-01-21 17:41:20 +0000

    Trim reason message

--- modules/monitor/src/main/scala/ebayapp/monitor/clients/HttpClient.scala
+++ modules/monitor/src/main/scala/ebayapp/monitor/clients/HttpClient.scala
@@ -21,7 +21,11 @@ final private class LiveHttpClient[F[_]: Async](
     private val backend: SttpBackend[F, Any]
 )(using
     C: Clock[F]
-) extends HttpClient[F]:
+) extends HttpClient[F] {
+  
+  extension [T](res: Response[T])
+    private def reason: String = s"HTTP ${res.code} ${res.statusText}".trim
+  
   def status(connection: Monitor.Connection.Http): F[MonitoringEvent.StatusCheck] =
     for
       start <- C.now
@@ -30,12 +34,12 @@ final private class LiveHttpClient[F[_]: Async](
         .method(Method(connection.method.toString.toUpperCase), uri"${connection.url.toString}")
         .readTimeout(connection.timeout)
         .send(backend)
-        .map(r => (if r.code.isSuccess then Monitor.Status.Up else Monitor.Status.Down) -> s"HTTP ${r.code} ${r.statusText}")
+        .map(r => (if r.code.isSuccess then Monitor.Status.Up else Monitor.Status.Down) -> r.reason)
         .timeoutTo(connection.timeout, (Monitor.Status.Down -> s"Request timed-out after ${connection.timeout}").pure[F])
         .handleError(e => Monitor.Status.Down -> e.getMessage)
       end <- C.now
     yield MonitoringEvent.StatusCheck(res._1, end.durationBetween(start), start, res._2)
-
+}
 object HttpClient:
   def make[F[_]: Async: Clock](backend: SttpBackend[F, Any]): F[HttpClient[F]] =
     Monad[F].pure(LiveHttpClient[F](backend))
