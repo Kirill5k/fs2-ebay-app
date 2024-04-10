05ed47df0fef945ae6c83a92c86cbdac06096147
kirill5k <immotional@aol.com>
2024-04-10 20:48:37 +0100

    Create RetailConfigRepository

--- modules/core/src/main/scala/ebayapp/core/repositories/Repositories.scala
+++ modules/core/src/main/scala/ebayapp/core/repositories/Repositories.scala
@@ -2,15 +2,19 @@ package ebayapp.core.repositories
 
 import cats.effect.kernel.Async
 import cats.syntax.functor.*
+import cats.syntax.flatMap.*
 import mongo4cats.database.MongoDatabase
 
 trait Repositories[F[_]]:
   def resellableItems: ResellableItemRepository[F]
+  def retailConfig: RetailConfigRepository[F]
 
 object Repositories:
   def make[F[_]: Async](database: MongoDatabase[F]): F[Repositories[F]] =
-    ResellableItemRepository.mongo(database).map { itemsRepo =>
-      new Repositories[F] {
-        override def resellableItems: ResellableItemRepository[F] = itemsRepo
-      }
+    for
+      items <- ResellableItemRepository.mongo(database)
+      rc    <- RetailConfigRepository.mongo(database)
+    yield new Repositories[F] {
+      override def resellableItems: ResellableItemRepository[F] = items
+      override def retailConfig: RetailConfigRepository[F]      = rc
     }
