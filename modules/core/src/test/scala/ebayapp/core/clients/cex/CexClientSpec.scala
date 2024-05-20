5ad31e31246923b4a59945c104480d7cb073ce21
kirill5k <immotional@aol.com>
2024-05-20 13:23:19 +0100

    Tests for withUpdatedSellPrices

--- modules/core/src/test/scala/ebayapp/core/clients/cex/CexClientSpec.scala
+++ modules/core/src/test/scala/ebayapp/core/clients/cex/CexClientSpec.scala
@@ -5,6 +5,7 @@ import cats.syntax.option.*
 import ebayapp.core.MockLogger.given
 import ebayapp.core.MockRetailConfigProvider
 import ebayapp.core.common.config.{CacheConfig, GenericRetailerConfig}
+import ebayapp.core.domain.ResellableItemBuilder
 import ebayapp.core.domain.search.*
 import kirill5k.common.cats.Clock
 import kirill5k.common.sttp.test.SttpWordSpec
@@ -30,45 +31,88 @@ class CexClientSpec extends SttpWordSpec {
   )
 
   val config = MockRetailConfigProvider.make[IO](cexConfig = Some(cexConfig))
-  
-  "CexGraphqlClient" should {
-    "find items" in {
-      val reqBody =
-        """{"requests":[
-          |{
-          |"indexName":"prod_cex_uk",
-          |"params":"query=gta 5 xbox&userToken=ecf31216f1ec463fac30a91a1f0a0dc3&facetFilters=%5B%5B%22availability%3AIn%20Stock%20Online%22%2C%22availability%3AIn%20Stock%20In%20Store%22%5D%5D"
-          |}
-          |]}""".stripMargin.replaceAll("\n", "")
 
-      val criteria = SearchCriteria("gta 5 xbox")
+  "CexGraphqlClient" when {
+    "search" should {
+      "find items" in {
+        val reqBody =
+          """{"requests":[
+            |{
+            |"indexName":"prod_cex_uk",
+            |"params":"query=gta 5 xbox&userToken=ecf31216f1ec463fac30a91a1f0a0dc3&facetFilters=%5B%5B%22availability%3AIn%20Stock%20Online%22%2C%22availability%3AIn%20Stock%20In%20Store%22%5D%5D"
+            |}
+            |]}""".stripMargin.replaceAll("\n", "")
 
-      val testingBackend: SttpBackend[IO, Any] = backendStub
-        .whenRequestMatchesPartial {
-          case r
-              if r.isPost && r.isGoingTo("cex.com/1/indexes/*/queries") && r
-                .hasBody(reqBody) && r.hasParams(cexConfig.queryParameters.get) =>
-            Response.ok(readJson("cex/search-graphql-success-response.json"))
-          case r => throw new RuntimeException(r.uri.toString)
+        val criteria = SearchCriteria("gta 5 xbox")
+
+        val testingBackend: SttpBackend[IO, Any] = backendStub
+          .whenRequestMatchesPartial {
+            case r
+                if r.isPost && r.isGoingTo("cex.com/1/indexes/*/queries") && r
+                  .hasBody(reqBody) && r.hasParams(cexConfig.queryParameters.get) =>
+              Response.ok(readJson("cex/search-graphql-success-response.json"))
+            case r => throw new RuntimeException(r.uri.toString)
+          }
+
+        val cexClient = CexClient.graphql[IO](config, testingBackend)
+
+        val result = cexClient.flatMap(_.search(criteria).compile.toList)
+
+        result.asserting { items =>
+          items.flatMap(_.itemDetails.fullName) mustBe List(
+            "Grand Theft Auto V (5)",
+            "Grand Theft Auto V (5) *2 Disc*",
+            "Grand Theft Auto V (2 Disc)",
+            "Grand Theft Auto V (5) Collector's Ed."
+          )
+          items.map(_.buyPrice) mustBe List(
+            BuyPrice(2425, 15, None),
+            BuyPrice(2045, 4, None),
+            BuyPrice(170, 20, None),
+            BuyPrice(3, 58, None)
+          )
         }
+      }
+    }
 
-      val cexClient = CexClient.graphql[IO](config, testingBackend)
+    "withUpdatedSellPrices" should {
+      val game1 = ResellableItemBuilder.makeVideoGame(
+        "GTA 5",
+        platform = Some("XBOX ONE"),
+        sellPrice = None,
+        foundWith = SearchCriteria("XBOX", category = Some("games-xbox-one-series-x"))
+      )
+      val game2 = ResellableItemBuilder.makeVideoGame(
+        "Spider-man",
+        platform = Some("PS4"),
+        sellPrice = None,
+        foundWith = SearchCriteria("PS4", category = Some("games-ps4-ps5"))
+      )
+      val game3 = ResellableItemBuilder.makeVideoGame(
+        "Overwatch",
+        platform = Some("XBOX ONE"),
+        sellPrice = None,
+        foundWith = SearchCriteria("XBOX", category = Some("games-xbox-one-series-x"))
+      )
 
-      val result = cexClient.flatMap(_.search(criteria).compile.toList)
+      "find resell prices" in {
+        val testingBackend: SttpBackend[IO, Any] = backendStub
+          .whenRequestMatchesPartial {
+            case r if r.isPost && r.isGoingTo("cex.com/1/indexes/*/queries") && r.hasParams(cexConfig.queryParameters.get) =>
+              Response.ok(readJson("cex/search-graphql-compound-success-response.json"))
+            case r => throw new RuntimeException(r.uri.toString)
+          }
 
-      result.asserting { items =>
-        items.flatMap(_.itemDetails.fullName) mustBe List(
-          "Grand Theft Auto V (5)",
-          "Grand Theft Auto V (5) *2 Disc*",
-          "Grand Theft Auto V (2 Disc)",
-          "Grand Theft Auto V (5) Collector's Ed."
-        )
-        items.map(_.buyPrice) mustBe List(
-          BuyPrice(2425, 15, None),
-          BuyPrice(2045, 4, None),
-          BuyPrice(170, 20, None),
-          BuyPrice(3, 58, None)
-        )
+        val result = for
+          client <- CexClient.graphql[IO](config, testingBackend)
+          updatedItems <- client.withUpdatedSellPrices(List(game1, game2, game3))
+        yield updatedItems
+
+        result.asserting { items =>
+          items(0).sellPrice mustBe Some(SellPrice(5, 8))
+          items(1).sellPrice mustBe Some(SellPrice(5, 8))
+          items(2).sellPrice mustBe None
+        }
       }
     }
   }
