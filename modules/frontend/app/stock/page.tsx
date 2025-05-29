af8b349fd3c1c530c19ff9438b87fd34c33a3f01
kirill5k <immotional@aol.com>
2025-05-29 11:01:30 +0100

    Stock list

--- modules/frontend/app/stock/page.tsx
+++ modules/frontend/app/stock/page.tsx
@@ -2,18 +2,22 @@
 
 import {useDealsStore} from '@/store/provider'
 import FilterAndSortPanel from '@/components/stock/filters'
+import StockList from '@/components/stock/list'
 
 export default function StockPage() {
   const {stock, stockSort, setStockSort, stockFilters, setStockFilters} = useDealsStore((state) => state)
 
   return (
-    <main className="p-2 md:p-8">
+    <main className="flex flex-col gap-4 p-2 md:p-8">
       <FilterAndSortPanel
         items={stock.items}
         sort={stockSort}
         onSortChange={setStockSort}
         filters={stockFilters}
         onFiltersChange={setStockFilters}
+      />
+      <StockList
+        items={stock.items}
       />
     </main>
   )
