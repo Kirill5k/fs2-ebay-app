5e390383c528c88e54830a2be4eca9e9780e9ddd
kirill5k <immotional@aol.com>
2025-05-30 19:45:09 +0100

    Install table

--- modules/frontend/app/deals/page.tsx
+++ modules/frontend/app/deals/page.tsx
@@ -3,12 +3,12 @@
 import {useDealsStore} from '@/store/provider'
 
 export default function StockPage() {
-  const {deals} = useDealsStore((state) => state)
+  const {deals, dealsFilters} = useDealsStore((state) => state)
 
   return (
-    <div className="p-8">
+    <main className="flex flex-col gap-4 p-2 md:p-8">
       <h1 className="text-2xl font-bold mb-4">Deals Information</h1>
       {deals.loading ? <p>Loading deals data...</p> : <p>Deals</p>}
-    </div>
+    </main>
   )
 }
