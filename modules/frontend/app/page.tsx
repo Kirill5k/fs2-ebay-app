0132eabf0ef04c89095c1eece1c299d2326cd4b7
kirill5k <immotional@aol.com>
2025-06-05 12:39:47 +0100

    Fix deployment

--- modules/frontend/app/page.tsx
+++ modules/frontend/app/page.tsx
@@ -4,7 +4,7 @@ import {useDealsStore} from '@/store/provider'
 import StockSummary from '@/components/stock/summary'
 
 export default function Home() {
-  const {dealsFilters, stock} = useDealsStore((state) => state)
+  const {stock} = useDealsStore((state) => state)
 
   return (
       <main className="flex flex-col gap-4 p-2 md:p-8">
--- modules/frontend/components/deals/summary/index.tsx
+++ modules/frontend/components/deals/summary/index.tsx
@@ -1,5 +1,3 @@
-import {Card, CardContent, CardTitle, CardDescription, CardHeader} from '@/components/ui/card'
-import {Badge} from '@/components/ui/badge'
 import {ResellableItem} from '@/store/state'
 
 const DealsSummary = ({items}: {items: ResellableItem[]}) => {
