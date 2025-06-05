969d4604655ab35612e07d2809a0b16090450aff
kirill5k <immotional@aol.com>
2025-06-05 12:33:56 +0100

    Deals summary placeholder

---
+++ modules/frontend/components/deals/summary/index.tsx
@@ -0,0 +1,12 @@
+import {Card, CardContent, CardTitle, CardDescription, CardHeader} from '@/components/ui/card'
+import {Badge} from '@/components/ui/badge'
+import {ResellableItem} from '@/store/state'
+
+const DealsSummary = ({items}: {items: ResellableItem[]}) => {
+
+  return (
+      <div></div>
+  )
+}
+
+export default DealsSummary
